01127c0d0a2a7e1a8ba543a7875ec021cd1d76e5
贾玉斌 <jiayubin@jiayubindemac-mini.local>
2016-02-20 17:01:42 +0800

    Initial Commit

---
+++ webar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  webar
+//
+//  Created by 贾玉斌 on 16/2/20.
+//  Copyright © 2016年 贾玉斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ webar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  webar
+//
+//  Created by 贾玉斌 on 16/2/20.
+//  Copyright © 2016年 贾玉斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
